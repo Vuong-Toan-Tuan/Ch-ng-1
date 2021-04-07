15f6d90eb9f16f0aff1176fefcdf46ad8ced9090
desod-vn <thangdotpro@gmail.com>
2021-04-07 14:47:17 +0700

    upload bai 1

--- Bai1/script.js
+++ Bai1/script.js
@@ -6,9 +6,11 @@ let content = `<h2>CHƯƠNG I. HTML</h2>
                 CSS là ngôn ngữ định kiểu , một bộ quy tắc để mô tả cách hiển thị của các đối tượng trên trang web
                 <h2>CHƯƠNG III. Javascript</h2>
                 <br />
+
                 Javascript là ngôn ngữ lập trình xử lý trang web động phía client
                 <h2>CHƯƠNG IV. JQUERY</h2>
                 <br />
+                
                 jQuery là một thư viện các hàm Javascript giúp nhà phát triển giảm thiểu số lượng mã lệnh khi viết code với Javascript
                 `;
 $('#content').html(content);
